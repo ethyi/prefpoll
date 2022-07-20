a66e75c836c940acde3d89608613a787b400f649
ethyi <ethyi@proton.me>
2022-07-20 09:51:18 -0700

    Fix: added host name to config file

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -8,23 +8,25 @@ if ! [ -x "$(command -v psql)" ]; then
 fi
 
 # Check if a custom user has been set, otherwise default to 'postgres'
-DB_USER="${POSTGRES_USER:=postgres}"
+DB_USER=${POSTGRES_USER:=postgres}
 # Check if a custom password has been set, otherwise default to 'password'
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-# Check if a custom database name has been set, otherwise default to 'prefpoll'
-DB_NAME="${POSTGRES_DB:=prefpoll}"
+# Check if a custom database name has been set, otherwise default to 'newsletter'
+DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-# Allow to skip Docker if a dockerized Postgres database is already running
-  docker run \
-      -e POSTGRES_USER=${DB_USER} \
-      -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-      -e POSTGRES_DB=${DB_NAME} \
-      -p "${DB_PORT}":5432 \
-      -d postgres \
-      postgres -N 1000
-      # ^ Increased maximum number of connections for testing purposes
+
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+docker run \
+-e POSTGRES_USER=${DB_USER} \
+-e POSTGRES_PASSWORD=${DB_PASSWORD} \
+-e POSTGRES_DB=${DB_NAME} \
+-p "${DB_PORT}":5432 \
+-d postgres \
+postgres -N 1000
+fi
 
 # Keep pinging Postgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
